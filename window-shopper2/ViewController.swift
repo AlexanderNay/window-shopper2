860514af317af982ec50c431fe3b4cae995c442a
AlexanderN <AlexanderN@Mac-mini-Aleksandr-2.local>
2017-09-19 07:55:47 +0300

    Initial Commit

---
+++ window-shopper2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper2
+//
+//  Created by AlexanderN on 19.09.17.
+//  Copyright © 2017 AlexanderN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
