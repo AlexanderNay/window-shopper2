ccdc0d8e156235744e6287da872552b6aafbfb30
AlexanderN <AlexanderN@Mac-mini-Aleksandr-2.local>
2017-09-20 12:01:05 +0300

    Programmatically  add "money sign" to text fields with current money symbol .

--- window-shopper2/View/CurrencyTextField.swift
+++ window-shopper2/View/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currensyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currensyLabel.backgroundColor = #colorLiteral(red: 0.6642242074, green: 0.6642400622, blue: 0.6642315388, alpha: 0.8539704623)
+        currensyLabel.textAlignment = .center
+        currensyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currensyLabel.layer.cornerRadius = 5.0
+        currensyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currensyLabel.text = formatter.currencySymbol
+        addSubview(currensyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -25,6 +40,8 @@ class CurrencyTextField: UITextField {
         backgroundColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2493043664)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if placeholder ==  nil {
             placeholder = " Test "
